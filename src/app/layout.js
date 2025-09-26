c7e3be7d059bf3dc0711287e6d6e9ebd285054e3
Alexis <piteralexis03@gmail.com>
2025-09-26 05:18:35 -0600

    Actualización rápida de la web

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,14 @@
-import "./globals.css"; // <- aquí va, obligatorio
+import "./globals.css"; 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/images/logor.png" />
+        <title>Rorosin</title>
+      </head>
       <body>
         <Header />
         <main>{children}</main>
